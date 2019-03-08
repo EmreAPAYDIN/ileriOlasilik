e64fb6e5cf99af38057b6dc5c8e628849cf55bab
EmreAPAYDIN <48322875+EmreAPAYDIN@users.noreply.github.com>
2019-03-08 15:37:13 +0300

    Update pigeonhole_algorithm.py
    Emre APAYDIN

--- pigeonhole_algorithm.py
+++ pigeonhole_algorithm.py
@@ -1,17 +1,30 @@
+#GÜVERCİN YUVASI PRENSİBİ
+#Standart 52 karttan oluşan bir iskambil destesinden 5 kart alırsak,
+#en az iki tanesi aynı türden kart olacaktır.kart türleri;kupa,karo,maça,sinek
 
+    #5 kartın her biri dört kart türünden birine aittir. 
+    #güvercin yuvası prensibine göre,iki yada daha fazla kart aynı kart türüne 
+    #ait olacaktır.
 import math
-# Python program to implement Pigeonhole Sort */ 
-  
-# source code : "https://en.wikibooks.org/wiki/ 
-#   Algorithm_Implementation/Sorting/Pigeonhole_sort" 
+
 def pigeonhole_sort(k): 
-    # size of range of values in the list  
-    # (ie, number of pigeonholes we need) 
+   
     if k>4:
-        pigeon =math.ceil(k/4)
+        pigeon =math.ceil(k/4) #güvercin yasası prensbinin genel formülü gereği
+        #yuvalara düşecek ortalama değer bulunur.Ve yukarı yuvarlanır.
         return pigeon
     else:
        print("bu ispat için en az 5 kart gerekmektedir!!!") 
+    
+     
+while 1:
+    k = int (input("elinizde kaç kart var? :"))
+    if k<0 or k>52 :
+        print("0-52 arasında değer giriniz")
+    else:
+        print("Aynı türden kağıt sayısı:", end = ' ') 
+        b=pigeonhole_sort(k)     
+        print(b)
     
      
     
@@ -20,11 +33,3 @@ def pigeonhole_sort(k):
   
     
 
-
-# In[14]:
-
-while 1:
-    k = int (input("elinizde kaç kart var? :"))
-    print("Aynı türden kağıt sayısı:", end = ' ') 
-    b=pigeonhole_sort(k)     
-    print(b)
